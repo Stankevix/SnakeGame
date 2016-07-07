c214bb7cab6b1ce3724c1d194568802918f085d2
Stankevix <gabriel_stankevix@hotmail.com>
2016-07-07 11:26:33 -0300

    Snake Game
    BackGround and body

--- main.py
+++ main.py
@@ -11,6 +11,8 @@
 
 import pygame
 import random
+import sys
+
 # ===================INIT MODULES========================
 
 #initiate all the module
@@ -31,15 +33,23 @@ pygame.display.set_caption('Snake Game')
 
 snakeImage = pygame.image.load('snake.png')
 foodImage = pygame.image.load('Food.png')
+background = pygame.image.load('background.jpg')
 
 clock = pygame.time.Clock()
+f = pygame.font.SysFont('comicsansms', 40);
+
+#Size of snake
+x = [290, 290]
+y = [290, 270]
 
 
-x = (display_width * 0.45)
-y = (display_height * 0.8)
+#x = (display_width * 0.45)
+#y = (display_height * 0.8)
 x_food = random.uniform(0,600)
 y_food = random.uniform(0,800)
 
+score = 0
+speed = 1
 crashed = False
 flag_Left = False
 flag_Right = False
@@ -49,72 +59,94 @@ eat = False
 
 # =======================FUNCTIONS==============================
 
+#This function draws the food :)
 def food():
 	gameDisplay.blit(foodImage,(x_food,y_food))
-	
+
+#This function draws the snake	
 def snake(x,y):
-	#drawing background stuffs
-	gameDisplay.blit(snakeImage,(x,y))
+	for i in range(0, len(x)):
+		gameDisplay.blit(snakeImage,(x[i],y[i]))
 
-while not crashed:
+#When the snake dies, this function shows the final scores and finish the game
+def die(screen, score):
+	f=pygame.font.SysFont('Arial', 30);
+	t=f.render('Your score was: '+str(score), True, (0, 0, 0));
+	screen.blit(t, (10, 270));
+	pygame.display.update();
+	pygame.time.wait(2000);
+	sys.exit(0)
 
-	#get any event (click/keyboard)
-	#create a list of events per frame
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			crashed = True	
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x-=1
-				flag_UP = False
-				flag_Left = True
-				flag_Right = False
-				flag_Down = False
-			elif event.key == pygame.K_RIGHT:
-				x+=1
-				flag_UP = False
-				flag_Left = False
-				flag_Right = True
-				flag_Down = False
-			elif event.key == pygame.K_UP:
-				y-=1
-				flag_UP = True
-				flag_Left = False
-				flag_Right = False
-				flag_Down = False
-			elif event.key == pygame.K_DOWN:
-				y+=1
-				flag_UP = False
-				flag_Left = False
-				flag_Right = False
-				flag_Down = True
-	
-	#Colision
-	print int(x), int(y), int(x_food), int(y_food)
-	if (int(x) <= int(x_food+1) or int(x)>= int(x_food-1)) and (int(y) <= int(y_food+1) and int(y) <= int(y_food-1)):
-		print "Colid"
-		x_food = random.uniform(0,600)
-		y_food = random.uniform(0,800)
 
-	gameDisplay.fill(white)
-	food()
-	snake(x,y)
-	pygame.display.update()
-	#define the number of frame per second
-	clock.tick(60)
+#Main Function, Loop Game and Collide
+if __name__ == '__main__':
 
-	#Movement of the snake
-	if flag_UP == True:
-		y-=1
-	elif flag_Down == True:
-		y+=1
-	elif flag_Right == True:
-		x+=1
-	elif flag_Left == True:
-		x-=1
+	while not crashed:
 
-	
+		#get any event (click/keyboard)
+		#create a list of events per frame
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				crashed = True	
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x-=1
+					flag_UP = False
+					flag_Left = True
+					flag_Right = False
+					flag_Down = False
+				elif event.key == pygame.K_RIGHT:
+					x+=1
+					flag_UP = False
+					flag_Left = False
+					flag_Right = True
+					flag_Down = False
+				elif event.key == pygame.K_UP:
+					y-=1
+					flag_UP = True
+					flag_Left = False
+					flag_Right = False
+					flag_Down = False
+				elif event.key == pygame.K_DOWN:
+					y+=1
+					flag_UP = False
+					flag_Left = False
+					flag_Right = False
+					flag_Down = True
 		
+		#Colision
+		#if (int(x) <= int(x_food+2) and int(x)>= int(x_food-2)) and (int(y) <= int(y_food+2) and int(y) <= int(y_food)):
+			#x_food = random.uniform(0, 400)
+			#y_food = random.uniform(0,400)
+			#speed+=0.2
+
+		#if int(x) >= 800 or int(y) >= 600 or int(x) <= 0 or int(y) <= 0:
+			#crashed = True	
+
+
+		#gameDisplay.fill(white)
+		gameDisplay.blit(background,(0,0))
+		food()
+		snake(x,y)
+		
+
+		#define the number of frame per second
+		clock.tick(60)
+
+		#Movement of the snake
+		if flag_UP == True:
+			y-=(1*speed)
+		elif flag_Down == True:
+			y+=(1*speed)
+		elif flag_Right == True:
+			x+=(1*speed)
+		elif flag_Left == True:
+			x-=(1*speed)
+
+		displayScore = f.render('SCORE: '+str(score), True, (0, 0, 0))
+		gameDisplay.blit(displayScore, (10, 10))
+
+		pygame.display.update()
 
 pygame.quit()
 quit()
